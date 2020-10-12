086b65d9e4567c7259fee73a8380c801cbdfc940
Anton Kablukov <antoha7@gmail.com>
2020-10-12 12:29:31 +0200

    feat: add NotFoundError

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,11 +1,12 @@
 const DB = require('../../common/DB/users');
+const { NotFoundError } = require('../../common/errors/notFoundError');
 
 const getAll = async () => await DB.getAllUsers();
 
 const getById = async id => {
   const user = await DB.getUser(id);
 
-  if (!user) throw new Error(`The user with id ${id} was not found`);
+  if (!user) throw new NotFoundError(`The user with id ${id} was not found`);
 
   return user;
 };
@@ -13,7 +14,9 @@ const getById = async id => {
 const create = async user => {
   const newUser = await DB.createUser(user);
 
-  if (!newUser) throw new Error(`Could not create user with id ${user.id}`);
+  if (!newUser) {
+    throw new NotFoundError(`Could not create user with id ${user.id}`);
+  }
 
   return newUser;
 };
@@ -21,7 +24,7 @@ const create = async user => {
 const update = async (id, data) => {
   const user = await DB.updateUser(id, data);
 
-  if (!user) throw new Error('Could not update user with id ${id}');
+  if (!user) throw new NotFoundError('Could not update user with id ${id}');
 
   return user;
 };
@@ -29,7 +32,7 @@ const update = async (id, data) => {
 const remove = async id => {
   const user = await DB.removeUser(id);
 
-  if (!user) throw new Error('Could not remove user with id ${id}');
+  if (!user) throw new NotFoundError('Could not remove user with id ${id}');
 
   return user;
 };
