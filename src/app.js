16735c9925b4aeec32dcc57a6021b2d45102716d
Anton Kablukov <antoha7@gmail.com>
2020-10-14 21:33:08 +0200

    feat: add validation error and global error handling

--- src/app.js
+++ src/app.js
@@ -31,4 +31,12 @@ app.use((err, req, res, next) => {
   next();
 });
 
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+  });
+
 module.exports = app;
